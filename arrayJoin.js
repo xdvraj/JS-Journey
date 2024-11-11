3cc3e00ea8ab875692df2f9a05a1dc1659b20f15
VRAJ <139940705+xdvraj@users.noreply.github.com>
2024-11-11 21:36:33 +0530

    Array Join

---
+++ arrayJoin.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 4, 5];
+const joined = numbers.join('-');
+console.log(joined);
+
+const message = 'hello this is my first message';
+const parts = message.split(' ');
+console.log(parts);
+
+const combined = parts.join('-');
+console.log(combined);
